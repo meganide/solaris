c1df94a85ddb191637f275a94b3341884d36c335
Meganide <renashassan@hotmail.com>
2022-11-26 12:16:58 +0100

    added try catch to http requests

--- js/modules/httpRequests.js
+++ js/modules/httpRequests.js
@@ -4,22 +4,30 @@ const BASE_URL = 'https://fathomless-shelf-54969.herokuapp.com/';
 const API_KEY = await httpGetApiKey(); // top level await
 
 async function httpGetApiKey() {
-  let response = await fetch(`${BASE_URL}keys`, {
-    method: 'POST',
-  });
-  let data = await response.json();
+  try {
+    let response = await fetch(`${BASE_URL}keys`, {
+      method: 'POST',
+    });
+    let data = await response.json();
 
-  return data.key;
+    return data.key;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function httpGetBodies() {
-  let response = await fetch(`${BASE_URL}bodies`, {
-    method: 'GET',
-    headers: { 'x-zocom': API_KEY },
-  });
-  let data = await response.json();
+  try {
+    let response = await fetch(`${BASE_URL}bodies`, {
+      method: 'GET',
+      headers: { 'x-zocom': API_KEY },
+    });
+    let data = await response.json();
 
-  return data.bodies;
+    return data.bodies;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export { httpGetBodies };
